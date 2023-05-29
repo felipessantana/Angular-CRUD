2d60526f8498b802d61f86221a7ed512572f8a53
Felipe Santos de Santana <73258352+felipessantana@users.noreply.github.com>
2023-05-29 20:06:29 -0300

    import module for creating forms

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,7 +15,12 @@ import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {HttpClientModule} from '@angular/common/http';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { FormsModule } from '@angular/forms';
+import {MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 
@@ -41,6 +46,13 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatListModule,
     MatCardModule,
     MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+
+
     
   ],
   providers: [],
