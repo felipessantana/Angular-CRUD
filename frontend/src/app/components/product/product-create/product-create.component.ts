894046e66f95c635c9ea7ab942f64ac891d48c19
Felipe Santos de Santana <73258352+felipessantana@users.noreply.github.com>
2023-05-29 20:04:42 -0300

    Creation of the function to register products and cancel products

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,5 +1,8 @@
+import { empty } from 'rxjs';
+import { Product } from './../product.model';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
-
+import { Router} from '@angular/router';
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  product : Product ={
+    name: '',
+    price: null as any
   }
 
+  constructor(private productService: ProductService, private router: Router) { }
+
+  ngOnInit(): void {
+    
+  }
+  createProduct(): void{
+    this.productService.create(this.product).subscribe(() =>{
+      this.productService.showMessage('Produto Criado')
+      this.router.navigate(['/products'])
+    })
+    
+  }
+  cancel(): void{
+    this.router.navigate(['/products'])
+
+  }
 }
