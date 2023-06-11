45516e0df23527470ec504cad0b90bf22b1d7bfa
Felipe Santos de Santana <73258352+felipessantana@users.noreply.github.com>
2023-06-11 12:19:31 -0300

    Update product-read
    applying styling, creating database data connection with form fields

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,23 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product.model';
 import { ProductService } from '../product.service';
+import { Observable } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-product-read',
   templateUrl: './product-read.component.html',
-  styleUrls: ['./product-read.component.css']
+  styleUrls: ['./product-read.component.css'],
 })
 export class ProductReadComponent implements OnInit {
 
-  products: Product[] | undefined
+  products: Product[] = [];
+  displayedColumns: string[] = ['id', 'name', 'price', 'action'];
+  
+  
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) { 
+    
+  }
 
   ngOnInit(): void {
     
     this.productService.read().subscribe(products =>{
       this.products = products
-      console.log(products)
+      
+      
     })
   }
 
