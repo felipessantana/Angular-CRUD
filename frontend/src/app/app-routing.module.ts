6654ef8db5c10038bdff8fb97d7791759b953b64
Felipe Santos de Santana <73258352+felipessantana@users.noreply.github.com>
2023-06-11 12:29:05 -0300

    Update app-routing
    creating the route for products/create, update, delete

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import './views/home/home.component';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 
 const routes: Routes = [{
@@ -13,8 +15,16 @@ const routes: Routes = [{
   path: "products",
   component: ProductCrudComponent
 },{
-  path: "product/create",
+  path: "products/create",
   component: ProductCreateComponent
+},
+{
+  path: "products/update/:id",
+  component: ProductUpdateComponent
+},
+{
+  path: "products/delete/:id",
+  component: ProductDeleteComponent
 }
 ];
 
